3f7bb809007af3ad957d9d442a2eb46c3948b7a8
Portfolio_Repo <cvaldez0128@gmail.com>
2021-03-07 17:04:35 -0500

    changes

--- Portfolio1/exercise/src/components/PlankingExercise/index.js
+++ Portfolio1/exercise/src/components/PlankingExercise/index.js
@@ -3,7 +3,7 @@ import React from "react"
 export default class Planking extends React.Component {
     render() {
         return (
-            <></>
+            <><h1>WHO'S READY 10 MINUTES OF PLANKING?</h1></>
         )
     }
 }
